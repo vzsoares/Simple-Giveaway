a82307415281b07022429efc45bbf77f07c9d54a
VzsoaresDesktopBH <jogolots2@gmail.com>
2021-10-30 16:14:52 -0300

    functionality update + bugfixes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import "./App.css";
 import $ from "jquery";
+import React, { useState } from "react";
 
 function App() {
+  const [winnersArray, setWinnersArray] = useState([]);
+  const [winnersExist, setWinnersExist] = useState(false);
   function Get_winners() {
     // variables
     var range_start = Number($("#number-range-start").val());
@@ -9,18 +12,49 @@ function App() {
     var amount_of_winners = Number($("#number-winners").val());
     var winners = [];
     // functions
+
+    // no negative number allowed
+    if (range_end <= 0 || range_start < 0 || amount_of_winners <= 0) {
+      window.alert("Invalid input!");
+      return;
+    }
+    // avoid more winners than prize
+    if (amount_of_winners >= range_end - range_start + 2) {
+      $(".amount-of-winners").css("color", "red");
+      $("#number-winners").css("color", "red");
+      window.alert("Too many winners");
+      return;
+    } else {
+      $(".amount-of-winners").css("color", "");
+      $("#number-winners").css("color", "");
+    }
+    // get random number
     for (let index = 0; index < amount_of_winners; ) {
-      var random = Math.floor(Math.random() * (range_end - range_start));
-      if (!winners.includes(random)) {
+      var random = Math.round(Math.random() * range_end);
+      if (
+        !winners.includes(random) &&
+        random <= range_end &&
+        random >= range_start
+      ) {
         index++;
         winners.push(random);
       }
     }
+    // get numbers for copy
+    const winners_array = winners;
+    setWinnersArray(winners_array.filter((x) => isFinite(x)));
+    // insert winners in DOM
     winners.forEach((e) => {
       winners.push(`<div class='winner-block'>${e}</div>`);
     });
     $(".winners").html(winners.filter((x) => isNaN(x)).join(""));
+    setWinnersExist(true);
   }
+  // copy to clipboard and alert user
+  const copyWinners = () => {
+    navigator.clipboard.writeText(winnersArray.join());
+    alert("Winners Copied!!!");
+  };
   return (
     <div className='App'>
       <div className='hero'>
@@ -33,6 +67,7 @@ function App() {
             id='number-range-start'
             className='number-input'
             defaultValue='0'
+            min='0'
           />
           <p>to</p>
           <input
@@ -40,6 +75,7 @@ function App() {
             id='number-range-end'
             className='number-input'
             defaultValue='1000'
+            min='1'
           />
         </div>
         <div className='number-amount'>
@@ -49,6 +85,7 @@ function App() {
             id='number-winners'
             className='number-input'
             defaultValue='1'
+            min='1'
           />
         </div>
         <div className='number-results'>
@@ -61,6 +98,16 @@ function App() {
           </button>
           <p>Winner(s):</p>
           <div className='winners'></div>
+          {winnersExist && (
+            <button
+              type='button'
+              className='generate-winners'
+              onClick={copyWinners}
+              style={{ marginTop: "1.5rem" }}
+            >
+              Copy Winner(s)
+            </button>
+          )}
         </div>
         <div className='footer'>
           <div className='space'></div>
